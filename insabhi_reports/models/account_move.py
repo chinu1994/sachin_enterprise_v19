568fb6c0d7bcfbc048dda193342f6a9a74c7bfa6
insabhi-sukanya <sukanya@insabhi.com>
2026-06-03 11:54:01 +0530

    -- po number add with label,serial number from invoice lines.

---
+++ insabhi_reports/models/account_move.py
@@ -0,0 +1,20 @@
+from odoo import models, fields, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    po_no = fields.Char(string="PO No")
+    manufacturing_no = fields.Char(string="Manufacturing No", compute="_compute_manufacturing_no")
+
+    def _compute_manufacturing_no(self):
+        for rec in self:
+            mo_name = False
+            sale_orders = rec.invoice_line_ids.mapped('sale_line_ids.order_id')
+            mos = self.env['mrp.production'].search([
+                ('origin', 'in', sale_orders.mapped('name'))
+            ], limit=1)
+
+            if mos:
+                mo_name = mos.name
+
+            rec.manufacturing_no = mo_name
--- insabhi_reports/models/stock_picking.py
+++ insabhi_reports/models/stock_picking.py
@@ -8,3 +8,9 @@ class StockPicking(models.Model):
     total_box = fields.Char(string="Total Box")
     box_type = fields.Char(string="Box Type")
     box_size = fields.Char(string='Box Size')
+    po_no = fields.Char(string="PO No", compute="_compute_po_no")
+
+    def _compute_po_no(self):
+        for rec in self:
+            invoice = rec.sale_id.invoice_ids.filtered(lambda inv: inv.po_no)[:1]
+            rec.po_no = invoice.po_no if invoice else False
