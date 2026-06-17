275a3235adf53feae18271aefc7ed37bbc68282e
chinu1994 <sachinburnawal01@gmail.com>
2026-06-17 22:27:40 +0530

    invoice customization

--- insabhi_invoice_custom/models/invoice.py
+++ insabhi_invoice_custom/models/invoice.py
@@ -1,11 +1,29 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    insabhi_packing_list_doc_ids = fields.Many2many('ir.attachment', string='Packing Lists')
-    insabhi_LR_copy_doc_ids = fields.Many2many('ir.attachment', string='LR Copy')
-    insabhi_MTC_doc_ids = fields.Many2many('ir.attachment', string='MTC')
+    insabhi_packing_list_doc_ids = fields.Many2many(
+        'ir.attachment',
+        'account_move_packing_list_rel',
+        'move_id',
+        'attachment_id',
+        string='Packing Lists'
+    )
+
+    insabhi_LR_copy_doc_ids = fields.Many2many(
+        'ir.attachment',
+        'account_move_lr_copy_rel',
+        'move_id',
+        'attachment_id',
+        string='LR Copy'
+    )
+
+    insabhi_MTC_doc_ids = fields.Many2many(
+        'ir.attachment',
+        'account_move_mtc_rel',
+        'move_id',
+        'attachment_id',
+        string='MTC'
+    )
