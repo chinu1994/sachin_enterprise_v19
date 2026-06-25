cbd36c2fb88af01d1bb4ad09f8b7ddefc1838281
insabhi-brishti <brishti@insabhi.com>
2026-06-25 18:56:40 +0530

    add increment decrement in invoice line

--- insabhi_reports/models/account_move.py
+++ insabhi_reports/models/account_move.py
@@ -26,4 +26,24 @@ class AccountMoveLine(models.Model):
     no_of_lebel = fields.Integer(
         string='No of Label',
         default=0,
+        states={},
+        copy=True,
     )
+
+    def action_increment_lebel(self):
+        for rec in self:
+            self.env.cr.execute(
+                "UPDATE account_move_line SET no_of_lebel = no_of_lebel + 1 WHERE id = %s",
+                (rec.id,)
+            )
+            rec.invalidate_recordset(['no_of_lebel'])
+
+    def action_decrement_lebel(self):
+        for rec in self:
+            if rec.no_of_lebel > 0:
+                self.env.cr.execute(
+                    "UPDATE account_move_line SET no_of_lebel = no_of_lebel - 1 WHERE id = %s",
+                    (rec.id,)
+                )
+                rec.invalidate_recordset(['no_of_lebel'])
+
